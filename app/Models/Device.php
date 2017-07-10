58ddac7e0b865c5f2472560b9996be5c753976b8
Mohyaddin Alaoddin <mo7y.66@gmail.com>
2017-07-10 16:37:10 +0300

    Installed Laravel Passport for API authentication.

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -1,8 +1,12 @@
 <?php namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
-class Device extends Model{
+class Device extends Authenticatable{
+    use Notifiable, HasApiTokens;
+
     protected $fillable = ['user_id', 'notification_token', 'api_token', 'last_ip'];
 
 	public function user(){
